663b59e6b1f1625d7e49eb5a08df791233564244
Agorbanoff <Alexandargorbanov@gmail.com>
2025-03-20 21:02:47 +0200

    removed query from SessionRepository

--- springboot/src/main/java/org/example/persistence/repository/SessionRepository.java
+++ springboot/src/main/java/org/example/persistence/repository/SessionRepository.java
@@ -11,8 +11,5 @@ import java.util.Optional;
 public interface SessionRepository extends BaseRepository<SessionEntity> {
     boolean existsBySessionString(String sessionString);
     Optional<SessionEntity> findBySessionString(String sessionString);
-
-    @Query("SELECT p FROM Payment p WHERE p.user.sessionString = :sessionString AND p.date BETWEEN :startDate AND :endDate")
-    List<Payment> findPaymentsByUserAndDateRange(String sessionString, LocalDate startDate, LocalDate endDate);
-
+    
 }
