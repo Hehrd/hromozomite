a9067161a33b0d1cc8a580c5780c481396b2cee8
Agorbanoff <Alexandargorbanov@gmail.com>
2025-03-20 14:17:01 +0200

    added entity and repository

---
+++ springboot/src/main/java/org/example/persistence/model/UserAccountEntity.java
@@ -0,0 +1,23 @@
+package org.example.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user_account")
+@Data
+public class UserAccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+}
+
