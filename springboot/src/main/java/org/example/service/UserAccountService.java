b6dca0940732a7388294d384a0bb9cd83594650c
Agorbanoff <Alexandargorbanov@gmail.com>
2025-03-20 14:26:59 +0200

    added sign up and log in method

--- springboot/src/main/java/org/example/service/UserAccountService.java
+++ springboot/src/main/java/org/example/service/UserAccountService.java
@@ -1,4 +1,37 @@
 package org.example.service;
 
+import lombok.Data;
+import org.example.controller.model.UserCredentialsDTO;
+import org.example.persistence.model.UserAccountEntity;
+import org.example.persistence.repository.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserAccountService {
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    public void signup(UserCredentialsDTO userCredentialsDTO) {
+        UserAccountEntity userAccountEntity = new UserAccountEntity();
+        userAccountEntity.setUsername(userAccountEntity.getUsername());
+        userAccountEntity.setPassword(userAccountEntity.getPassword());
+        userAccountEntity.setEmail(userAccountEntity.getEmail());
+        userAccountRepository.save(userAccountEntity);
+    }
+
+    public void login(UserCredentialsDTO userCredentialsDTO){
+        String email = userCredentialsDTO.getEmail();
+        String password = userCredentialsDTO.getPassword();
+        UserAccountEntity userAccountEntity = userAccountRepository.findByEmailAndPassword(email, password).orElse(null);
+
+        if (userAccountEntity == null) {
+            throw new RuntimeException("No user found with the provided credentials.");
+        }
+
+    }
+    public void logout(){
+
+    }
 }
