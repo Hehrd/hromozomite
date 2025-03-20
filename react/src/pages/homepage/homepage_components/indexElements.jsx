48635ce4f1f4c1949cb1daefb9d4847b939feb7c
Martin-Boyanov <mboyanov09@gmail.com>
2025-03-21 01:49:49 +0200

    added links

--- react/src/pages/homepage/homepage_components/indexElements.jsx
+++ react/src/pages/homepage/homepage_components/indexElements.jsx
@@ -1,23 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 import "./indexElements.css";
-import MoneySpentChart from "./moneySpentChart.jsx";
+import MoneySpentChart from "./moneySpentChart.jsx"; // Import the chart component
 
 const IndexElements = () => {
   return (
     <div className="tabs-container">
-      {/* Money Spent */}
+      {/* Money Spent - Link to analyticsPage */}
       <div className="tab-box">
-        <h2>Money Spent</h2>
-        <MoneySpentChart /> {/* Line chart inside */}
+        <Link to="/analyticsPage" className="tab-link">
+          <h2>Money Spent</h2>
+          <MoneySpentChart /> {/* Line chart inside */}
+        </Link>
       </div>
 
-      {/* Friends Leaderboard */}
+      {/* Transactions today - Link to transactionsPage */}
       <div className="tab-box">
-        <h2>Transactions today</h2>
-        <p>Coming Soon...</p>
+        <Link to="/transactions" className="tab-link">
+          <h2>Transactions today</h2>
+          <p>Coming Soon...</p>
+        </Link>
       </div>
 
-      {/* Money Saved */}
+      {/* Money Saved - Placeholder content */}
       <div className="tab-box">
         <h2>Money Saved This Month</h2>
         <p>Coming Soon...</p>
