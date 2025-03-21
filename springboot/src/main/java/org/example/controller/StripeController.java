15175f0862d303e83654bf5d75fbfb77829bfd96
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-03-21 10:05:41 +0200

    Added cross origin to the stripe controller

--- springboot/src/main/java/org/example/controller/StripeController.java
+++ springboot/src/main/java/org/example/controller/StripeController.java
@@ -13,13 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/stripe")
+@CrossOrigin(origins = "http://localhost:5173")
 public class StripeController {
     @Value( "${stripe.api.key}")
     private String apiKey;
