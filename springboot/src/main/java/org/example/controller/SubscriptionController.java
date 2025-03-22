ead3fa820ed6405da4d98b3de66e69a751012535
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-03-22 02:03:32 +0200

    Added isLoggedIn method

--- springboot/src/main/java/org/example/controller/SubscriptionController.java
+++ springboot/src/main/java/org/example/controller/SubscriptionController.java
@@ -17,7 +17,7 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService subscriptionService;
 
-    @RequestMapping(value = "/subscription", method = RequestMethod.GET)
+    @RequestMapping(value = "/addsubscription", method = RequestMethod.GET)
     public ResponseEntity<String> subscribe(@RequestBody SubscriptionDTO subscriptionDTO, @CookieValue(value = "SESSION_STRING") String sessionString) throws UserNotFoundException {
         subscriptionService.saveSubscription(subscriptionDTO);
         return ResponseEntity
